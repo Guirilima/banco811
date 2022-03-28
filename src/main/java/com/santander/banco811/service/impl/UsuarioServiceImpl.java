1e3ad4186248a15e5c3ad31b779f7b756774c798
Guilherme Lima <46833577+Guirilima@users.noreply.github.com>
2022-03-28 19:50:18 -0300

    Update UsuarioServiceImpl.java

--- src/main/java/com/santander/banco811/service/impl/UsuarioServiceImpl.java
+++ src/main/java/com/santander/banco811/service/impl/UsuarioServiceImpl.java
@@ -10,6 +10,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.crossstore.ChangeSetPersister;
 import org.springframework.stereotype.Service;
 
+import java.util.Random
+
 import java.util.List;
 import java.util.Optional;
 
@@ -18,6 +20,8 @@ public class UsuarioServiceImpl implements UsuarioService {
 
     @Autowired
     UsuarioRepository usuarioRepository;
+    @Autowired
+    ContaRepository contaRepository;
 
     @Override
     public List<Usuario> getAll(String nome) {
@@ -58,5 +62,29 @@ public class UsuarioServiceImpl implements UsuarioService {
         var usuario = usuarioRepository.findById(id).orElseThrow();
 
         usuarioRepository.delete(usuario);
+    }
+    
+    @Override
+    public void createNewAccount (AccountRequest accountRequest, Integer id) {
+        var usuario = this.getById(id); // Validação se existe.
+        
+        //Criação da conta
+        Conta conta = new Conta();
+        conta.setUsuario(usuario); //Associação
+        
+        //Já que estamos recebendo CPF ...
+        conta.setTipoConta(TipoConta.PF);
+        conta.setSaldo(new BigDecimal("0"));//Conta zerada
+        
+        //Asociação pelo request
+        conta.setAgencia(accountRequest.getAgencia());
+        
+        //Lógica de última hora,
+        // Número aleatorio 0 á 678 + os três últimos número do CPF.
+        conta.setNumero(random.nextInt(678) + Integer.parseInt(StringUtils.right(usuario.getCpf(), 3)))
+        
+        contaRepository.save(conta);
+        //Sucesso.
+        //Retorno 200 ou poderia retorna o 201 + id no Path.        
     }
 }
